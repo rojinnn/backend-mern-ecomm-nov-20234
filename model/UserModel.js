934c9efd676d329d08e0704b8b336623f4312ba8
S0330B <sushilsharmasubedi@gmail.com>
2024-01-21 22:18:36 +0545

    Created a user validation schema using yup

--- model/UserModel.js
+++ model/UserModel.js
@@ -3,7 +3,5 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  age: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now },
 });
 module.exports = mongoose.model("user", userSchema);
