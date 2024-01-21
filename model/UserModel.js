00466417d243d6cc31db3f122963f2ca4c6400ed
S0330B <sushilsharmasubedi@gmail.com>
2024-01-21 22:10:32 +0545

    User Model created.

---
+++ model/UserModel.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  age: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model("user", userSchema);
