01b75461c5759ebfed41cfb4223e4cceb90425a1
S0330B <sushilsharmasubedi@gmail.com>
2024-01-23 21:58:06 +0545

    Api to get product id

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("./db/config");
 const User = require("./model/User");
+const Product = require("./model/Product");
 const app = express();
 const env = require("dotenv");
 env.config();
@@ -29,7 +30,29 @@ app.post("/login", async (req, res) => {
     res.status(404).send({ Message: "Error User Not Found" });
   }
 });
-// app.listen(5000);
+
+app.post("/add-product", async (req, res) => {
+  let product = new Product(req.body);
+  let result = await product.save();
+  res.status(200).send(result);
+});
+
+app.get("/products", async (req, res) => {
+  let products = await Product.find();
+  if (products.length > 0) {
+    res.status(200).send(products);
+  } else {
+    res.status(400).send({ result: "No product found" });
+  }
+});
+app.delete("/product/:id", async (req, res) => {
+  let result = await Product.deleteOne({ _id: req.params.id });
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(404).send("Product not found");
+  }
+});
 app.listen(process.env.PORT, () => {
   console.log("Serving at port number", process.env.PORT);
 });
